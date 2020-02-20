15cd65a109231753a9469878e449e5d36b574884
Jiw Surachat <jiw.surachat@gmail.com>
2020-02-20 11:27:05 +0700

    Update index.php
    Test Deploy

--- index.php
+++ index.php
@@ -4,5 +4,6 @@
  </head>
  <body>
  <?php echo '<p>Hello World-Happy Learning. This is wonderful! This is my first Jenkins PHP200 page using OpenShift Online</p>'; ?> 
+ <?php echo '<p>Test Deploy 20-02-20,11:26</p>'; ?> 
  </body>
 </html>
